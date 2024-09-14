e2b598ef5b9441ba752909894d7995bcd1de677a
Jjejjwwj <bzjznsmensjs@gmail.com>
2024-09-14 13:08:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '522202687279'
+global.nomerowner = ["522202687279"]
 
 // Apikey 
 
